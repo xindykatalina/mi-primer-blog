92f4c8073be5b378b0ebba6bffd36499ff4a3af0
xindykatalina <xindykatalina@gmail.com>
2017-02-12 17:17:06 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'blog/static'),)
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
